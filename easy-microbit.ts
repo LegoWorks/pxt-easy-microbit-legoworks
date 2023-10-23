026f634ca08cca2f0efc2250c6bbca52d0794250
LegoWorks <106318228+LegoWorks@users.noreply.github.com>
2023-10-23 18:39:29 +0200

    Actualizar pxt.json, easy-microbit.ts

---
+++ easy-microbit.ts
@@ -0,0 +1,66 @@
+
+//% color="#4169E1" icon="\uf11b"
+//% groups="['Digital pins', 'Analog pins']"
+//% block="Easy Microbit"
+
+namespace Easy_Microbit {
+
+    /**
+     * Block to control digital pins.
+     */
+    //% block="Fijar $pin a $value"
+    //% group="Digital pins"
+
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.width=20
+    //% pin.fieldOptions.columns=1
+
+    //% value.shadow="toggleOnOff"
+
+
+    export function setPin(pin: DigitalPin, value: boolean) {
+        value ? pins.digitalWritePin(pin, 1) : pins.digitalWritePin(pin, 0);
+    }
+
+    /**
+     * Block to read digital pins value.
+     */
+    //% block="Leer valor digital de $pin"
+    //% group="Digital pins"
+
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.width=20
+    //% pin.fieldOptions.columns=1
+    export function readPin(pin: DigitalPin): int8 {
+        return pins.digitalReadPin(pin)
+    }
+
+
+    /**
+     * Block to read analog pins value.
+     */
+    //% block="Leer valor analógico de $pin"
+    //% group="Analog pins"
+
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.width=20
+    //% pin.fieldOptions.columns=1
+    export function readAnalogPin(pin: AnalogPin) {
+        return pins.analogReadPin(pin)
+    }
+
+    /**
+     * Block to control analog pins.
+     */
+    //% block="Fijar $pin analógico a $value"
+    //% group="Analog pins"
+
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.width=20
+    //% pin.fieldOptions.columns=1
+
+    //% value.min=0 value.max=1023 value.defl=1 
+    export function writeAnalog(pin: AnalogPin, value: number) {
+        pins.analogWritePin(pin, value)
+    }
+}
