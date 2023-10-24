544697b1b9abfc576bf889503c3fbb7ddb2da77a
LegoWorks <106318228+LegoWorks@users.noreply.github.com>
2023-10-24 14:57:26 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-Easy_pins.setPin(DigitalPin.P0, true)
 basic.forever(function () {
-    if (Easy_pins.OnPinState(DigitalPin.P0, true)) {
-        basic.showIcon(IconNames.Heart)
-    } else {
-        basic.showIcon(IconNames.Tortoise)
-    }
+	
 })
