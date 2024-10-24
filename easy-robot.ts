4e7789cdcc05340c242cd0393a808da44e865628
LegoWorks <106318228+LegoWorks@users.noreply.github.com>
2024-10-24 17:40:08 +0200

    Actualizar pxt.json, main.ts, easy-robot.ts

---
+++ easy-robot.ts
@@ -0,0 +1,25 @@
+
+//% color="#4169E1" icon="\uf11b"
+//% groups="['Config', 'movement']"
+//% block="Easy robot"
+
+
+
+namespace Easy_robot {
+
+    /**
+     * Block to control digital pins.
+     */
+    //% block="Fijar $pin a $value"
+    //% group="Digital pins"
+
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.width=20
+    //% pin.fieldOptions.columns=1
+
+    //% value.shadow="toggleOnOff"
+
+
+}
+
+
