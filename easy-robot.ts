69590cecd309abb71040be0dbd52d033a8242381
LegoWorks <106318228+LegoWorks@users.noreply.github.com>
2025-01-17 15:44:57 +0100

    Actualizar main.blocks, easy-robot.ts

--- easy-robot.ts
+++ easy-robot.ts
@@ -1,36 +1,24 @@
 
 //% color="#4169E1" icon="\uf11b"
-//% groups="['Config', 'movement']"
+//% groups="['Config', 'Movement']"
 //% block="Easy robot"
 
 
 
 namespace Easy_robot {
 
-    /**
-     * Block to control digital pins.
-     */
-    //% block="Fijar $pin a $value"
-    //% group="Digital pins"
 
-    //% pin.fieldEditor="gridpicker"
-    //% pin.fieldOptions.width=20
-    //% pin.fieldOptions.columns=1
-
-    //% value.shadow="toggleOnOff"
 
 
     /**
-     * Block to read digital pins value.
+     * Runs the motor at the given speed
      */
-    //% block="Leer valor digital de $pin"
-    //% group="Digital pins"
 
-    //% pin.fieldEditor="gridpicker"
-    //% pin.fieldOptions.width=20
-    //% pin.fieldOptions.columns=1
-    export function readPin(pin: DigitalPin): int8 {
-        return pins.digitalReadPin(pin)
+    //% block="crickit run at $speed %"
+    //% group="Movement
+    //% speed.shadow="speedPicker"
+    export function run(speed: number) {
+
     }
 
 }
